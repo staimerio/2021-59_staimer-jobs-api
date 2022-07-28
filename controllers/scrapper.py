1386de196f1543cf033dbd6a9807a4a04375187b
Braian Staimer <braian.florian@>
2022-07-27 22:44:52 -0600

    fix scrapper torrents

--- controllers/scrapper.py
+++ controllers/scrapper.py
@@ -67,6 +67,7 @@ def scrapper_shows(req: Request, res: Response, next: Next):
     """Response the data to client"""
     res.ok(_result)
 
+
 def scrapper_series(req: Request, res: Response, next: Next):
     """Validate obligate params"""
     _validate = validate_obligate_fields({
@@ -124,6 +125,41 @@ def scrapper_torrents(req: Request, res: Response, next: Next):
         req.param('wp_password'),
         req.param('wp_url'),
         req.param('origin', "todotorrents"),
+        req.param('page', 1),
+    )
+    """Check if exist an error"""
+    if _result['valid'] is False:
+        return res.bad_request(_result)
+    """Response the data to client"""
+    res.ok(_result)
+
+
+def scrapper_hentai(req: Request, res: Response, next: Next):
+    """Validate obligate params"""
+    _validate = validate_obligate_fields({
+        u'wp_login': req.param('wp_login'),
+        u'wp_admin': req.param('wp_admin'),
+        u'wp_username': req.param('wp_username'),
+        u'wp_password': req.param('wp_password'),
+        u'wp_url': req.param('wp_url'),
+    })
+
+    """Check if has errors return a error response"""
+    if _validate["valid"] is False:
+        return res.bad_request(
+            error_response(
+                "The param {} is necesary.".format(_validate["error"])
+            )
+        )
+
+    """Get all novel from latests page"""
+    _result = scrapper.scrapper_hentai(
+        req.param('wp_login'),
+        req.param('wp_admin'),
+        req.param('wp_username'),
+        req.param('wp_password'),
+        req.param('wp_url'),
+        req.param('origin', "hentaila"),
         req.param('page', 1),
     )
     """Check if exist an error"""
